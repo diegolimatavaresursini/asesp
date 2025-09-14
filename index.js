849e83bc264ffb0e8f4a6685308c3ae13ca473c8
diegoltursini <73371155+diegolimatavaresursini@users.noreply.github.com>
2025-09-14 12:58:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i) {
+  index = (i + images.length) % images.length;
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+prevBtn.addEventListener('click', () => showSlide(index - 1));
+nextBtn.addEventListener('click', () => showSlide(index + 1));
